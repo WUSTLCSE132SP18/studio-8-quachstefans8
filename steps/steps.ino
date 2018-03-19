5d7199dce485857779e4ebaf5edd9c0339e2b07b
Stefan Quach <stefan.quach@wustl.edu>
2018-03-19 14:56:29 -0500

    Studio 8

--- steps/steps.ino
+++ steps/steps.ino
@@ -1,7 +1,10 @@
 #include <Wire.h>
 #include <SparkFun_MMA8452Q.h>
+#define BUFF_SIZE 10
 
 MMA8452Q accel;
+int count = 0;
+double buff[BUFF_SIZE][3];
 
 void setup() {
   Serial.begin(9600);
@@ -11,6 +14,11 @@ void setup() {
 void loop() {
   if (accel.available()) {
     accel.read();
+    double input[] = {accel.cx, accel.cy, accel.cz};
+    for(int i=0; i<3; i++){
+      buff[count%BUFF_SIZE][i] = input[i];
+    }
+    
     printCAccel();
     Serial.println();
   }
